cb8d8819ed70619a60cb303c991fd7b1632d1b82
erick-sudo <erickochieng766@gmail.com>
2023-04-12 02:46:00 +0300

    Active storage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_record_response
+
     def index
         @articles = Article.all
     end
@@ -8,6 +10,28 @@ class ArticlesController < ApplicationController
     end
 
     def new
-        
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to @article
+        else
+            pp @article.errors.full_messages
+            render :new, status: :unprocessable_entity
+        end
+        # Article.create!(article_params)
+        # redirect_to @article
+    end
+
+    private
+
+    def article_params
+        params.permit(:title, :body, :images)
+    end
+
+    def invalid_record_response(invalid)
+        render :new, status: :unprocessable_entity
     end
 end
